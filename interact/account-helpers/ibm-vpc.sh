f65ec1787c4e37c32e80e37c12257ce1ded98ea0
d3rn <84808889+d3rnn@users.noreply.github.com>
2024-11-05 18:22:41 -0600

    Update ibm-vpc.sh

--- interact/account-helpers/ibm-vpc.sh
+++ interact/account-helpers/ibm-vpc.sh
@@ -103,26 +103,30 @@ function create_apikey {
 }
 
 function specs {
+    echo -e -n "${BGreen}Printing available resource groups...\n${Color_Off}"
+    ibmcloud resource groups
+    echo -e -n "${BGreen}Please enter the resource groups to use (press enter for 'Default'): \n>> ${Color_Off}"
+    read resource_group
+    resource_group=${resource_group:-Default}
+
     echo -e -n "${Green}Printing available regions..\n${Color_Off}"
-    ibmcloud is regions
+    ibmcloud regions
     echo -e -n "${BGreen}Please enter your default region (press enter for 'us-south'): \n>> ${Color_Off}"
     read region
     region=${region:-us-south}
-    ibmcloud target -r $region
+    ibmcloud target -r $region -g $resource_group
 
     echo -e -n "${Green}Printing available zones in region selected..\n${Color_Off}"
     ibmcloud is zones
     echo -e -n "${BGreen}Please enter your default zone in region (press enter for 'us-south-1'): \n>> ${Color_Off}"
-    read region
-    zone=${zone:-us-south1}
-    ibmcloud target -z $zone
+    read zone
+    zone=${zone:-us-south-1}
 
     echo -e -n "${BGreen}Printing available instance profiles in zone/region...\n${Color_Off}"
     ibmcloud is instance-profiles
     echo -e -n "${BGreen}Please enter instance profile for your device to use (press enter for 'bx2-2x8'): \n>> ${Color_Off}"
     read profile
     profile=${profile:-bx2-2x8}
-
 }
 
 function setVPC {
@@ -130,12 +134,20 @@ function setVPC {
     ibmcloud is vpcs
     echo -e -n "${Green}What VPC would you like to use? (press enter to create a new one): \n>> ${Color_Off}"
     read vpc
-    vpc=${vpc:-$vpc}
+    if [[ "$vpc" == "" ]]; then
+     name="axiom-$(date +%m-%d-%H-%M-%S-%1N)"
+     new_vpc_data=$(ibmcloud is vpcc $name --output json)
+     echo -e -n "${Green}Created VPC${Color_Off}"
+     echo $new_vpc_data | jq
+     vpc=$(echo $new_vpc_data | jq -r .id)
+    else
+     vpc=$vpc
+   fi
 
 }
 
 function setprofile {
-    data="{\"ibm_cloud_api_key\":\"$ibm_cloud_api_key\",\"region\":\"$region\",\"zone\":\"$zone\",\"provider\":\"ibm-vpc\",\"vpc\":\"$vpc\"}"
+    data="{\"ibm_cloud_api_key\":\"$ibm_cloud_api_key\",\"default_size\":\"$profile\",\"resource_group\":\"$resource_group\",\"region\":\"$region\",\"zone\":\"$zone\",\"provider\":\"ibm-vpc\",\"vpc\":\"$vpc\"}"
     echo -e "${BGreen}Profile settings below:${Color_Off}"
     echo $data | jq ' .ibm_cloud_api_key = "********"'
     echo -e "${BWhite}Press enter to save these to a new profile, type 'r' to start over.${Color_Off}"
@@ -161,23 +173,24 @@ do
         "SSO")
             echo "Attempting to authenticate with SSO!"
             ibmcloud login --no-region --sso
-            getUsernameAPIkey
             create_apikey
             specs
+            setVPC
             setprofile
             break
             ;;
         "Username & Password")
             ibmcloud login --no-region
-            getUsernameAPIkey
             create_apikey
             specs
+            setVPC
             setprofile
             break
             ;;
         "API Keys")
             apikeys
             specs
+            setVPC
             setprofile
             break
             ;;
